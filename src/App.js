26a194bbb05cf5ac076aace5cec55947c47f9783
viccabezos <cabezosvic@gmail.com>
2021-03-30 12:31:06 +0200

    insertion contact El Barto

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import './components/contact';
 import './components/contact.css';
+import Contact from './components/contact';
 
 function App() {
   return (
-    
+   <div className="App">
+     < Contact />
+   </div> 
   );
 }
 
--- src/components/contact.js
+++ src/components/contact.js
@@ -1,2 +1,22 @@
 import './contact.css'
 
+const name = "Barto";
+const avatar ="https://placekitten.com/g/200/300";
+const status = true;
+const statusText = status ? "online" : "offline";
+const statusIndicator = status ? "status-online" : "status-offline";
+
+function Contact() {
+  return (
+    <div className="Contact">
+      <img className="avatar" src={avatar} alt="test" />
+      <h3 className="name">{name}</h3>
+      <div className="status">
+        <p className="status-text">{statusText}</p>
+        <div className={statusIndicator}></div>
+      </div>
+    </div>
+  );
+}
+
+export default Contact;
